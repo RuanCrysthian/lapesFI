38221d1a1af70a8b09358302c96c34e0c4c74960
Ruan Crysthian <ruanlima155@gmail.com>
2023-06-21 18:09:35 -0300

    build: setting the database

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,7 +3,7 @@ import { Pool } from 'pg';
 export const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'teste',
+  database: 'lapesfi',
   password: 'admin',
   port: 5432,
 });
