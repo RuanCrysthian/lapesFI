18e554e42c1099cd245708aaebe01125c0d7a7c0
Ruan Crysthian <ruanlima155@gmail.com>
2023-06-22 19:56:27 -0300

    build: add route for get all resources

--- src/routes/resourceRoutes.ts
+++ src/routes/resourceRoutes.ts
@@ -4,5 +4,6 @@ import { ResourceController } from '../controllers/resourceController';
 const router = express.Router();
 
 router.post('/', ResourceController.createResource);
+router.get('/', ResourceController.getAllResource);
 
 export default router;
