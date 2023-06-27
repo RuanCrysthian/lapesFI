c0b09fa93db60cf5f408a55c4615394b8c6c6aab
Ruan Crysthian <ruanlima155@gmail.com>
2023-06-27 11:39:25 -0300

    build: create route for get all Erros

--- src/routes/errorRoutes.ts
+++ src/routes/errorRoutes.ts
@@ -4,5 +4,6 @@ import { ErrorController } from '../controllers/errorController';
 const router = express.Router();
 
 router.post('/', ErrorController.create);
+router.get('/', ErrorController.getAllErrors);
 
 export default router;
