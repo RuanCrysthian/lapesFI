5f7a0a7d3ee2a2591b802555ed4c4a5c7db8992c
Ruan Crysthian <ruanlima155@gmail.com>
2023-06-28 19:18:15 -0300

    build: create the method to find a Error by resource_uuid

--- src/models/error.ts
+++ src/models/error.ts
@@ -129,4 +129,76 @@ export class Error {
 
     return errors;
   }
+
+  static async getErrorsByResourceUUID(resourceUUID: string): Promise<Error[]> {
+    const client = await pool.connect();
+    const errors: Error[] = [];
+
+    try {
+      const queryText = 'SELECT * FROM error WHERE resource_uuid = $1';
+      const result = await client.query(queryText, [resourceUUID]);
+
+      if (result && result.rows.length > 0) {
+        for (const row of result.rows) {
+          let error: Error | null = null;
+
+          switch (row.type_of_error) {
+            case 'bias':
+              error = new Error(
+                row.resource_uuid,
+                new Bias(),
+                row.error_duration,
+                parseFloat(row.capability_value),
+              );
+              break;
+            case 'drift':
+              error = new Error(
+                row.resource_uuid,
+                new Drift(),
+                row.error_duration,
+                parseFloat(row.capability_value),
+              );
+              break;
+            case 'loss accuracy':
+              error = new Error(
+                row.resource_uuid,
+                new LossAccuracy(),
+                row.error_duration,
+                parseFloat(row.capability_value),
+              );
+              break;
+            case 'calibration error':
+              error = new Error(
+                row.resource_uuid,
+                new CalibrationError(),
+                row.error_duration,
+                parseFloat(row.capability_value),
+              );
+              break;
+            case 'freezing':
+              error = new Error(
+                row.resource_uuid,
+                new Freezing(),
+                row.error_duration,
+                parseFloat(row.capability_value),
+              );
+              break;
+            default:
+              console.warn('Tipo de erro desconhecido:', row.type_of_error);
+          }
+
+          if (error) {
+            errors.push(error);
+          }
+        }
+      }
+    } catch (error) {
+      console.error('Erro ao recuperar os Erros:', error);
+      throw error;
+    } finally {
+      client.release();
+    }
+
+    return errors;
+  }
 }
