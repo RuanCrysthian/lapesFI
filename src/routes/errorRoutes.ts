d3c15741aff5da9caf9c450bf5843a43233b030a
Ruan Crysthian <ruanlima155@gmail.com>
2023-06-28 19:18:47 -0300

    build: create the route error/:uuid

--- src/routes/errorRoutes.ts
+++ src/routes/errorRoutes.ts
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/', ErrorController.create);
 router.get('/', ErrorController.getAllErrors);
+router.get('/:uuid', ErrorController.getErrorByUUID);
 
 export default router;
