4da918262b28ca09e6cde87136c65b755edfe40c
RuanCrysthian <ruanlima155@gmail.com>
2023-07-09 18:53:52 -0300

    feat: create route for add data to sensors

--- src/routes/resourceRoutes.ts
+++ src/routes/resourceRoutes.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 router.post('/', ResourceController.createResource);
 router.get('/', ResourceController.getAllResource);
 router.get('/:uuid', ResourceController.getResourceByUUID);
+router.delete('/:uuid', ResourceController.deleteResource);
+router.post('/:uuid/data', ResourceController.saveSensorData);
 
 export default router;
