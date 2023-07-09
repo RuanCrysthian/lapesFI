2eb5fcd1def0195764feed65c2ed5965e3ac2a8e
RuanCrysthian <ruanlima155@gmail.com>
2023-07-09 18:48:48 -0300

    feat: create table sensor_value and refac others tables

--- src/database/migrations/create_tables.ts
+++ src/database/migrations/create_tables.ts
@@ -1,38 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { Knex } from 'knex';
 
-export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable('resource', function (table) {
-    table.string('uuid').primary();
-    table.string('description', 255).notNullable();
-    table.string('resource_environment', 255).notNullable();
-  });
+export async function up(knex: Knex): Promise<void> {}
 
-  console.log('Create the Resource table');
-
-  await knex.schema.createTable('capability', function (table) {
-    table.string('name', 255).notNullable();
-    table.double('value').notNullable();
-    table.string('resource_uuid').unsigned();
-    table.foreign('resource_uuid').references('resource.uuid');
-  });
-
-  console.log('Create the Capability table');
-
-  await knex.schema.createTable('error', function (table) {
-    table.string('error_uuid').primary();
-    table.string('resource_uuid').unsigned();
-    table.string('type_of_error');
-    table.integer('error_duration');
-    table.decimal('capability_value', 10, 3);
-    table.decimal('capability_error', 10, 3);
-    table.foreign('resource_uuid').references('resource.uuid');
-  });
-
-  console.log('Create the Error table');
-}
-
-export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('capability');
-  await knex.schema.dropTableIfExists('resource');
-  await knex.schema.dropTable('error');
-}
+export async function down(knex: Knex): Promise<void> {}
---
+++ src/database/migrations/create_tables_after_refac.ts
@@ -0,0 +1,50 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('resource', function (table) {
+    table.string('uuid').primary();
+    table.string('description');
+    table.string('location');
+  });
+
+  console.log('Create the Resource table');
+
+  await knex.schema.createTable('capability', function (table) {
+    table.string('uuid').primary();
+    table.string('name');
+    table.string('function');
+    table.string('description');
+    table.string('resource_uuid').unsigned();
+    table.foreign('resource_uuid').references('resource.uuid');
+  });
+
+  console.log('Create the Capability table');
+
+  await knex.schema.createTable('error', function (table) {
+    table.string('error_uuid').primary();
+    table.string('resource_uuid').unsigned();
+    table.string('type_of_error');
+    table.decimal('capability_value', 10, 3);
+    table.decimal('capability_error', 10, 3);
+    table.foreign('resource_uuid').references('resource.uuid');
+  });
+
+  console.log('Create the Error table');
+
+  await knex.schema.createTable('sensor_value', function (table) {
+    table.increments('id').primary();
+    table.decimal('value', 10, 3);
+    table.string('date');
+    table.string('capability_uuid').unsigned();
+    table.foreign('capability_uuid').references('capability.uuid');
+  });
+
+  console.log('Create the SensorValue table');
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('sensor_value');
+  await knex.schema.dropTable('capability');
+  await knex.schema.dropTable('error');
+  await knex.schema.dropTable('resource');
+}
