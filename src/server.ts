384a1a26dabee88c997f7373e1821407f727d193
RuanCrysthian <ruanlima155@gmail.com>
2023-07-09 18:43:39 -0300

    feat: add port from .env

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import resourceRoutes from './routes/resourceRoutes';
 import errorRoutes from './routes/errorRoutes';
+import sensor_valuesRoutes from './routes/sensor_valueRoutes';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-const port = 4000;
+const port: number = parseInt(process.env.PORT ?? '', 10);
 
 app.use(bodyParser.json());
 
@@ -14,6 +18,7 @@ app.get('/', (req, res) => {
 
 app.use('/resources', resourceRoutes);
 app.use('/errors', errorRoutes);
+app.use('/capabilities', sensor_valuesRoutes);
 
 app.listen(port, () => {
   console.log(`Server on ${port}`);
