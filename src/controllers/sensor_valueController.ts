214bae4675505782242bbe2d25bf7204d41fcf8b
RuanCrysthian <ruanlima155@gmail.com>
2023-07-09 18:49:41 -0300

    feat: create the Sensor Value Controller

---
+++ src/controllers/sensor_valueController.ts
@@ -0,0 +1,7 @@
+import pool from '../database/db';
+import { Request, Response } from 'express';
+import { Resource } from '../models/resource';
+import { Capability } from '../models/capability';
+import { SensorValue } from '../models/sensor_value';
+
+export class SensorValueController {}
