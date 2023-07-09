985112d80382e0317e7b183ab2c67720d241ceaf
RuanCrysthian <ruanlima155@gmail.com>
2023-07-09 18:48:01 -0300

    refac: att the class Resource

--- src/models/resource.ts
+++ src/models/resource.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { QueryResult } from 'pg';
 import pool from '../database/db';
 import { Capability } from './capability';
@@ -8,38 +7,51 @@ export class Resource {
   uuid: string;
   description: string;
   capabilities: Capability[];
-  resourceEnvironment: string;
+  location: string;
 
   constructor(
     description: string,
     capabilities: Capability[],
-    resourceEnvironment: string,
+    location: string,
   ) {
     this.uuid = uuidv4();
     this.description = description;
     this.capabilities = capabilities || [];
-    this.resourceEnvironment = resourceEnvironment;
+    this.location = location;
   }
 
-  // Função para salvar um recurso no banco de dados
   async save(): Promise<void> {
-    const queryText =
-      'INSERT INTO resource (uuid, description, resource_environment) VALUES ($1, $2, $3)';
-    const values = [this.uuid, this.description, this.resourceEnvironment];
-
     try {
       const client = await pool.connect();
-      await client.query(queryText, values);
-      for (const capability of this.capabilities) {
-        const capabilityQueryText =
-          'INSERT INTO capability (name, value, resource_uuid) VALUES ($1, $2, $3)';
-        const capabilityValues = [capability.name, capability.value, this.uuid];
-        await client.query(capabilityQueryText, capabilityValues);
+
+      try {
+        // Inserir o Resource
+        const resourceInsertQuery =
+          'INSERT INTO resource (uuid, description, location) VALUES ($1, $2, $3)';
+        const values = [this.uuid, this.description, this.location];
+        await client.query(resourceInsertQuery, values);
+
+        // Inserir as Capability
+        for (const capability of this.capabilities) {
+          const capabilityInsertQuery =
+            'INSERT INTO capability (uuid, name, function, description, resource_uuid) VALUES ($1, $2, $3, $4, $5)';
+          const capabilityInsertValues = [
+            capability.uuid,
+            capability.name,
+            capability.function,
+            capability.description,
+            this.uuid,
+          ];
+          await client.query(capabilityInsertQuery, capabilityInsertValues);
+        }
+      } catch (error) {
+        console.error('Erro ao salvar o Resource:', error);
+        throw error;
+      } finally {
+        client.release();
       }
-      client.release();
     } catch (error) {
-      console.error('Erro ao salvar o Resource:', error);
-      throw error;
+      console.error('Error occurred:', error);
     }
   }
 
@@ -48,7 +60,7 @@ export class Resource {
 
     try {
       const queryText = `
-        SELECT r.uuid, r.description, r.resource_environment, c.name, c.value
+        SELECT r.uuid, r.description, r.location, c.uuid as capability_uuid, c.name
         FROM resource r
         LEFT JOIN capability c ON r.uuid = c.resource_uuid
       `;
@@ -60,16 +72,14 @@ export class Resource {
         const resourceUuid = row.uuid;
 
         if (!resourceMap.has(resourceUuid)) {
-          const resource = new Resource(row.description, [], row.resource_environment);
+          const resource = new Resource(row.description, [], row.location);
           resource.uuid = resourceUuid;
           resourceMap.set(resourceUuid, resource);
         }
 
-        if (resourceMap.has(resourceUuid)) {
-          const capability = {
-            name: row.name,
-            value: row.value,
-          };
+        if (row.capability_uuid) {
+          const capability = new Capability(row.name, row.description);
+          capability.uuid = row.capability_uuid;
           resourceMap.get(resourceUuid)?.capabilities.push(capability);
         }
       }
@@ -85,37 +95,12 @@ export class Resource {
     }
   }
 
-  static async delete(uuid: string): Promise<void> {
-    const client = await pool.connect();
-
-    try {
-      // Deletar as capacidades do recurso
-      const capabilityQueryText = 'DELETE FROM capabilities WHERE resource_uuid = $1';
-      const capabilityValues = [uuid];
-
-      await client.query(capabilityQueryText, capabilityValues);
-
-      // Deletar o recurso
-      const queryText = 'DELETE FROM resource WHERE uuid = $1';
-      const values = [uuid];
-
-      await client.query(queryText, values);
-
-      await client.query('COMMIT');
-    } catch (error) {
-      await client.query('ROLLBACK');
-      throw error;
-    } finally {
-      client.release();
-    }
-  }
-
   static async getByID(uuid: string): Promise<Resource | null> {
     const client = await pool.connect();
 
     try {
       const queryText = `
-        SELECT r.uuid, r.description, r.resource_environment, c.name, c.value
+        SELECT r.uuid, r.description, r.location, c.uuid as capability_uuid, c.name
         FROM resource r
         LEFT JOIN capability c ON r.uuid = c.resource_uuid
         WHERE r.uuid = $1
@@ -128,16 +113,14 @@ export class Resource {
         const resourceUuid = row.uuid;
 
         if (!resourceMap.has(resourceUuid)) {
-          const resource = new Resource(row.description, [], row.resource_environment);
+          const resource = new Resource(row.description, [], row.location);
           resource.uuid = resourceUuid;
           resourceMap.set(resourceUuid, resource);
         }
 
-        if (resourceMap.has(resourceUuid)) {
-          const capability = {
-            name: row.name,
-            value: row.value,
-          };
+        if (row.capability_uuid) {
+          const capability = new Capability(row.name, row.description);
+          capability.uuid = row.capability_uuid;
           resourceMap.get(resourceUuid)?.capabilities.push(capability);
         }
       }
@@ -147,6 +130,35 @@ export class Resource {
       return resource;
     } catch (error) {
       console.error('Erro ao recuperar o Resource:', error);
+      throw error;
+    } finally {
+      client.release();
+    }
+  }
+
+  static async delete(uuid: string): Promise<void> {
+    const client = await pool.connect();
+
+    try {
+      await client.query('BEGIN');
+
+      // Deletar as capacidades do recurso
+      console.log('entrei no delete');
+      const capabilityQueryText =
+        'DELETE FROM capability WHERE resource_uuid = $1';
+      const capabilityValues = [uuid];
+      await client.query(capabilityQueryText, capabilityValues);
+
+      console.log('estou deletando resource');
+      // Deletar o recurso
+      const queryText = 'DELETE FROM resource WHERE uuid = $1';
+      const values = [uuid];
+      await client.query(queryText, values);
+
+      await client.query('COMMIT');
+    } catch (error) {
+      console.log('estou no catch');
+      await client.query('ROLLBACK');
       throw error;
     } finally {
       client.release();
