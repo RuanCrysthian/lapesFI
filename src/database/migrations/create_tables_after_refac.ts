f4f05f065baffa08dcf867988f32d88c818ed57f
RuanCrysthian <ruanlima155@gmail.com>
2023-07-10 15:11:04 -0300

    refac: delete in cascate

--- src/database/migrations/create_tables_after_refac.ts
+++ src/database/migrations/create_tables_after_refac.ts
@@ -15,7 +15,10 @@ export async function up(knex: Knex): Promise<void> {
     table.string('function');
     table.string('description');
     table.string('resource_uuid').unsigned();
-    table.foreign('resource_uuid').references('resource.uuid');
+    table
+      .foreign('resource_uuid')
+      .references('resource.uuid')
+      .onDelete('CASCADE');
   });
 
   console.log('Create the Capability table');
@@ -25,17 +28,23 @@ export async function up(knex: Knex): Promise<void> {
     table.string('type_of_error');
     table.decimal('sensor_value', 10, 3);
     table.decimal('sensor_error', 10, 3);
-    table.foreign('capability_uuid').references('capability.uuid');
+    table
+      .foreign('capability_uuid')
+      .references('capability.uuid')
+      .onDelete('CASCADE');
   });
 
-  console.log('Create the Error table');
+  console.log('Create the Fault table');
 
   await knex.schema.createTable('sensor_value', function (table) {
     table.increments('id').primary();
     table.decimal('value', 10, 3);
     table.string('date');
     table.string('capability_uuid').unsigned();
-    table.foreign('capability_uuid').references('capability.uuid');
+    table
+      .foreign('capability_uuid')
+      .references('capability.uuid')
+      .onDelete('CASCADE');
   });
 
   console.log('Create the SensorValue table');
@@ -43,7 +52,7 @@ export async function up(knex: Knex): Promise<void> {
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.dropTable('sensor_value');
+  await knex.schema.dropTable('fault');
   await knex.schema.dropTable('capability');
-  await knex.schema.dropTable('error');
   await knex.schema.dropTable('resource');
 }
