6e826d04b2617157b805c803e4fb6ea4addb6af2
RuanCrysthian <ruanlima155@gmail.com>
2023-07-17 10:28:41 -0300

    build: add new dependencies to project

--- src/controllers/sensor_valueController.ts
+++
@@ -1,7 +0,0 @@
-import pool from '../database/db';
-import { Request, Response } from 'express';
-import { Resource } from '../models/resource';
-import { Capability } from '../models/capability';
-import { SensorValue } from '../models/sensor_value';
-
-export class SensorValueController {}
